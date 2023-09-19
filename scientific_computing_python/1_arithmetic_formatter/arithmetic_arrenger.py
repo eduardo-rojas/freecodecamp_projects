0ae5a9d439b7748910f8f07e7a671f29092b622c
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-09-19 14:26:46 -0700

    Completed Project #1 Arithmetic Formatter - Scientific Computing with python Certification. 100% Tests Pass

--- scientific_computing_python/1_arithmetic_formatter/arithmetic_arrenger.py
+++ scientific_computing_python/1_arithmetic_formatter/arithmetic_arrenger.py
@@ -1,52 +1,165 @@
 import itertools
 
+
 def arithmetic_arranger(problems, show_results=False):
-    if len(problems) > 5:
-        return "Error: Too many problems."
+  # Variables that store parts of each problem in 'problems'
+  top_operator = ""
+  bottom_operator = ""
+  separator = ""
+  result_string = ""
+  # Handle rule: Five problems limit
+  if len(problems) > 5:
+    return "Error: Too many problems."
+
+  # For loop to process each given operation
+  for i in range(len(problems)):
+    # Handle Addition - Exceptions
+    if problems[i].find("+") != -1:
+      
+      # Split string into a list with two elements
+      problem_split = problems[i].split("+")
+      # Obtain left and right operands and remove white space
+      left_operand = problem_split[0]
+      right_operand = problem_split[1]
+      left_operand = left_operand.strip()
+      right_operand = right_operand.strip()
+      
+      # Rule: integer with no more than 4 digits
+      if (len(left_operand) > 4) or (len(right_operand) > 4):
+        return "Error: Numbers cannot be more than four digits."
+      
+      try:
+        left_operand = int(left_operand)
+        right_operand = int(right_operand)
+        current_result = left_operand + right_operand
+      except:
+        return "Error: Numbers must only contain digits."
+      
+      # Assign the 'separator' length to determine the number of dashes
+      # and create right-aligned dinamic spacing
+      largest_digit = 0
+      dinamic_space_top = 0
+      dinamic_space_bottom = 0
+      dinamic_space_result = 0
+
+      
+      if left_operand > right_operand:
+        largest_digit = len(str(left_operand))
+        dinamic_space_top = len(str(left_operand)) + 2
+        dinamic_space_bottom = len(str(left_operand)) + 1
+        dinamic_space_result = len(str(left_operand)) + 2
+      if right_operand > left_operand:
+        largest_digit = len(str(right_operand))
+        dinamic_space_top = len(str(right_operand)) + 2
+        dinamic_space_bottom = len(str(right_operand)) + 1
+        dinamic_space_result = len(str(right_operand)) + 2
+      if len(str(right_operand)) == len(str(left_operand)):
+        largest_digit = len(str(right_operand))
+        dinamic_space_top = len(str(right_operand)) + 2
+        dinamic_space_bottom = len(str(right_operand)) + 1
+        dinamic_space_result = len(str(right_operand)) + 2
+
+      current_separator = "-" * (largest_digit + 2)
+
+      # Handle Addition - String Format Result
+      space_between = "    " 
+      if i == len(problems) - 1:
+        space_between = ""
+      else:
+        space_between = "    " 
+
+      top_operator += "{left_operand:>{dinamic_space}}".format(
+          left_operand=left_operand,
+          dinamic_space=dinamic_space_top) + space_between
+      bottom_operator += "+{right_operand:>{dinamic_space}}".format(
+          right_operand=right_operand,
+          dinamic_space=dinamic_space_bottom) + space_between
+      separator += current_separator + space_between
+      result_string += "{current_result:>{dinamic_space}}".format(
+          current_result=current_result,
+          dinamic_space=dinamic_space_result) + space_between
+    # Handle Subtraction
+    elif problems[i].find("-") != -1:
+      # Split string into a list with two elements
+      problem_split = problems[i].split("-")
+      # Obtain left and right operands and remove white space
+      left_operand = problem_split[0]
+      right_operand = problem_split[1]
+      left_operand = left_operand.strip()
+      right_operand = right_operand.strip()
+      # Rule: integer with no more than 4 digits
+      if (len(left_operand) > 4) or (len(right_operand) > 4):
+        return "Error: Numbers cannot be more than four digits."
+      
+      try:
+        left_operand = int(left_operand)
+        right_operand = int(right_operand)
+        current_result = left_operand - right_operand
+      except:
+        return "Error: Numbers must only contain digits."
+      
     
-    i = 0
-    while i <= (len(problems) - 1 ):
-        if problems[i].find("+"):
-            problem_split = problems[i].split("+")
-            left_operand = problem_split[0]
-            right_operand = problem_split[1]
-            left_operand = left_operand.strip()
-            right_operand = right_operand.strip()
-            
-            if (len(left_operand) > 4) or (len(right_operand) > 4):
-                return "Error: Numbers cannot be more than four digits."
-            
-            try:
-                left_operand = int(left_operand)
-                right_operand = int(right_operand)
-            except:
-                return "Error: Numbers must only contain digits."
+      # Assign the 'separator' length to determine the number of dashes
+      # and create right-aligned dinamic spacing
+      largest_digit = 0
+      dinamic_space_top = 0
+      dinamic_space_bottom = 0
+      dinamic_space_result = 0
 
-            # if not left_operand[0].isdigit():
-            #     return "Error: Numbers must only contain digits."
-            # if not right_operand[0].isdigit():
-            #     return "Error: Numbers must only contain digits."
-        elif problems[i].find("-"):
-            problem_split = problems[i].split("-")
-            left_operand = problem_split[0]
-            right_operand = problem_split[1]
-            left_operand = left_operand.strip()
-            right_operand = right_operand.strip()
-            if (len(left_operand) > 4) or (len(right_operand) > 4):
-                return "Error: Numbers cannot be more than four digits."
+      if left_operand > right_operand:
+        largest_digit = len(str(left_operand))
+        dinamic_space_top = len(str(left_operand)) + 2
+        dinamic_space_bottom = len(str(left_operand)) + 1
+        dinamic_space_result = len(str(left_operand)) + 2
+      if right_operand > left_operand:
+        largest_digit = len(str(right_operand))
+        dinamic_space_top = len(str(right_operand)) + 2
+        dinamic_space_bottom = len(str(right_operand)) + 1
+        dinamic_space_result = len(str(right_operand)) + 2
+      if len(str(right_operand)) == len(str(left_operand)):
+        largest_digit = len(str(right_operand))
+        dinamic_space_top = len(str(right_operand)) + 2
+        dinamic_space_bottom = len(str(right_operand)) + 1
+        dinamic_space_result = len(str(right_operand)) + 2
 
-            try:
-                left_operand = int(left_operand)
-                right_operand = int(right_operand)
-            except:
-                return "Error: Numbers must only contain digits."
-            
-            # if not left_operand[0].isdigit():
-            #     return "Error: Numbers must only contain digits."
-            # if not right_operand[0].isdigit():
-            #     return "Error: Numbers must only contain digits."
-        else: 
-            return "Error: Operator must be '+' or '-'."
-    i += 1
+      current_separator = "-" * (largest_digit + 2)
 
-    return "Test - End of Function"
+      
+      # Handle Subtraction - String Format Result
+      space_between = "    " 
+      if i == len(problems) - 1:
+        space_between = ""
+      else:
+        space_between = "    " 
+
+      top_operator += "{left_operand:>{dinamic_space}}".format(
+          left_operand=left_operand,
+          dinamic_space=dinamic_space_top) + space_between
+      bottom_operator += "-{right_operand:>{dinamic_space}}".format(
+          right_operand=right_operand,
+          dinamic_space=dinamic_space_bottom) + space_between
+      separator += current_separator + space_between
+      result_string += "{current_result:>{dinamic_space}}".format(
+          current_result=current_result,
+          dinamic_space=dinamic_space_result) + space_between
+    else:
+      # Rule: If operators '+' or '-' are not found
+      return "Error: Operator must be '+' or '-'."
+  top_operator += "\n"
+  bottom_operator += "\n"
+  
+  if show_results:
+    separator += "\n"
+    arithmetic_arranged_string = "{top_operator}{bottom_operator}{separator}{result_string}".format(
+        top_operator=top_operator,
+        bottom_operator=bottom_operator,
+        separator=separator,
+        result_string=result_string)
+  else:
+    arithmetic_arranged_string = "{top_operator}{bottom_operator}{separator}".format(
+        top_operator=top_operator,
+        bottom_operator=bottom_operator,
+        separator=separator)
+
+  # End of function - Return final string
+  return arithmetic_arranged_string
--- scientific_computing_python/1_arithmetic_formatter/main.py
+++ scientific_computing_python/1_arithmetic_formatter/main.py
@@ -1,7 +1,7 @@
 """
 Scientific Computing with Python Certification | freeCodeCamp
-Project 1 
-# Arithmetic Formatter
+Project #1 
+Arithmetic Formatter
 Author: Carlos Eduardo Hoyos Rojas
 """
 
@@ -9,4 +9,18 @@ from arithmetic_arrenger import arithmetic_arranger
 
 
 
-print(arithmetic_arranger(["584 + 321"]))
+print(arithmetic_arranger(["584 + 321", "5512 + 212", " 24 +  999", " 5 + 2"]))
+
+print(arithmetic_arranger( ["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"], True) )
+
+print(arithmetic_arranger(["3801 - 2", "123 + 49"]))
+
+print(arithmetic_arranger(["1 + 2", "1 - 9380"]))
+
+print(arithmetic_arranger(["3 + 855", "3801 - 2", "45 + 43", "123 + 49"]))
+
+print(arithmetic_arranger(["11 + 4", "3801 - 2999", "1 + 2", "123 + 49", "1 - 9380"]))
+
+print(arithmetic_arranger(["3 + 855", "988 + 40"], True))
+
+print(arithmetic_arranger(['32 - 698', '1 - 3801', '45 + 43', '123 + 49', '988 + 40'], True))
