7aabe0ee7642d0cca33c0906ed836bba759d6cf6
Eduardo Rojas <carlos.edhr@protonmail.com>
2023-10-11 15:27:45 -0700

    Completed Polygon Area Calculator project #4 of the Scientific computing with python certification.

--- scientific_computing_python/4_polygon_area_calculator/main.py
+++ scientific_computing_python/4_polygon_area_calculator/main.py
@@ -1,28 +1,18 @@
 # This entrypoint file to be used in development. Start by reading README.md
-from shape_calculator import Rectangle, Square
+import shape_calculator
 from unittest import main
 
-first_rectangle = Rectangle(10, 3)
+rect = shape_calculator.Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
 
-print("First rectangle Width: ", first_rectangle.width)
-print("First rectangle Height: ", first_rectangle.height)
-print("First rectangle Area: ", first_rectangle.get_area())
-print("First rectangle Perimeter: ", first_rectangle.get_perimeter())
-print(first_rectangle)
-print(first_rectangle.get_picture())
-#initial tests
-# rect = shape_calculator.Rectangle(5, 10)
-# print(rect.get_area())
-# rect.set_width(3)
-# print(rect.get_perimeter())
-# print(rect)
-
-# sq = shape_calculator.Square(9)
-# print(sq.get_area())
-# sq.set_side(4)
-# print(sq.get_diagonal())
-# print(sq)
-
+sq = shape_calculator.Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
 
 # Run unit tests automatically
-#main(module='test_module', exit=False)
+main(module='test_module', exit=False)
--- scientific_computing_python/4_polygon_area_calculator/shape_calculator.py
+++ scientific_computing_python/4_polygon_area_calculator/shape_calculator.py
@@ -14,9 +14,15 @@ class Rectangle:
         return self.width * self.height
     
     def get_perimeter(self):
+        return (self.width * 2 + self.height * 2) 
+    
+    def get_diagonal(self):
         return (self.width ** 2 + self.height ** 2) ** .5
     
     def get_picture(self):
+        if (self.width > 50) or (self.height > 50):
+            return 'Too big for picture.'
+        
         rect_picture = ""
         width_dynamic = self.width - 1
 
@@ -26,22 +32,28 @@ class Rectangle:
         
         rect_picture += "\n"
         
+        # CHECKPOINT
+        # if self.width == self.height:
+        #     rect_picture += "\n"
+        
         #Fill the inside of the rectangle: second loop
         for i in range(self.height - 2 ):
             for j in range(self.width):
                 rect_picture += "*"
+            rect_picture += "\n"
+        
            
         
-        rect_picture += "\n"
         
         #fill the bottom of the rectangle: third loop
         for i in range(self.width):
             rect_picture += "*"
+        rect_picture += "\n"
 
         return rect_picture 
     
     def get_amount_inside(self, shape):
-        return None
+        return int(self.get_area() / shape.get_area())
     
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
@@ -51,7 +63,21 @@ class Rectangle:
 
 
 
-class Square:
+class Square(Rectangle):
     def __init__(self, length):
-        Rectangle.__init__(self, length, length)
+        super().__init__(length, length)
         self.length = length
+    
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+        self.length = side
+    
+    def set_width(self, new_width):
+        self.length  = new_width
+
+    def set_height(self, new_height):
+        self.length  = new_height
+
+    def __str__(self):
+        return f"Square(side={self.length})"
